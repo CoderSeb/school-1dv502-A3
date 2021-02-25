7afae9074d61f89a046bc4d8e64111d9fa48cf26
Sebastian <sa224ny@student.lnu.se>
2021-02-25 12:01:48 +0100

    Updated Tile.java to pass the tests

--- src/main/java/monopoly/Tile.java
+++ src/main/java/monopoly/Tile.java
@@ -12,24 +12,25 @@ public abstract class Tile {
   // a tile knows what objects (players) are currently on the tile, these are
   // never used for anything else than comparing object references
   // thus we can store them at the highest level of abstraction - Object instances
-  ArrayList<Object> playersOnTile;  
+  ArrayList<Object> playersOnTile;
 
   protected Tile() {
     // TODO: set the prev and next references correctly
     // the next and prev links should point to the object created
 
-    next = prev = null; // this is not correct remove
+    next = prev = this;
 
     playersOnTile = new ArrayList<>();
   }
 
   protected Tile(Tile prevTile) {
     // TODO: set the prev and next references correctly
-    // the object created should be inserted into the linked list as the next tile after prevTile
+    // the object created should be inserted into the linked list as the next tile
+    // after prevTile
 
-    prev = next = prevTile; // this is not correct, remove
-    prevTile.next = prevTile.prev = this; // this is not correct, remove
-
+    this.next = prevTile.next;
+    prevTile.next = next.prev = this;
+    this.prev = prevTile;
     playersOnTile = new ArrayList<>();
   }
 
@@ -42,35 +43,35 @@ public abstract class Tile {
   }
 
   /**
-  * Checks if a player is on the tile.
-  */
+   * Checks if a player is on the tile.
+   */
   public boolean isOnTile(Player player) {
     return playersOnTile.contains(player);
   }
 
   /**
-  * Called when a player moves over a tile.
-  */
+   * Called when a player moves over a tile.
+   */
   public abstract void visit(Player player);
 
   /**
-  * Called when a player stops on a tile.
-  * Subclasses need to call super if overridden.
-  */
+   * Called when a player stops on a tile. Subclasses need to call super if
+   * overridden.
+   */
   public void stoppedOn(Player player) {
     playersOnTile.add(player);
   }
 
   /**
-  * Called when a player moves away from the tile stopped on.
-  * Subclasses need to call super if overridden.
-  */
+   * Called when a player moves away from the tile stopped on. Subclasses need to
+   * call super if overridden.
+   */
   public void startOn(Player player) {
     playersOnTile.remove(player);
   }
 
   /**
-  * Called when a player wants to buy a tile.
-  */
+   * Called when a player wants to buy a tile.
+   */
   public abstract boolean buy(Player player);
 }
